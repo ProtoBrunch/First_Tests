579cca27c5310de0a07147ec6781da62f14965af
ProtoBrunch <robin.berberat@gmail.com>
2017-03-18 11:22:44 +0100

    Initial Tests
    Let's see how this works

---
+++ src/HelloPlace.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Robin Berberat on 18.03.2017.
+ */
+public class HelloPlace {
+
+    HelloPlace(){
+        System.out.println("This is text from Hello Place.");
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+/**
+ * Created by Robin Berberat on 18.03.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Some more output");
+        HelloPlace hello = new HelloPlace();
+    }
+}
